a2d91ad8a7a8f2740e378ae4bdacb7afb95db75d
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2021-01-09 10:15:47 +0530

    first commit

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <drogon/drogon.h>
+
+#include "api/v1/login.h"
+#include "api/v1/register.h"
+
+using namespace drogon;
+
+int main() {
+    app().registerHandler("/api/v1/register", &api::v1::registration::doRegister);
+    app().loadConfigFile("../config.json");
+    app().run();    
+}
